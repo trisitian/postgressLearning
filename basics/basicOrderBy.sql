3010a4ff896a0130b10e2397ebe61ba49513ef15
Tristian Labanowich <tristian.labanowich@gmail.com>
2025-04-06 20:05:41 -0400

    shortest 5 movies

--- basics/basicOrderBy.sql
+++ basics/basicOrderBy.sql
@@ -3,3 +3,8 @@ SELECT DISTINCT customer_id, payment_date FROM payment
 WHERE amount > 0.0
 ORDER BY payment_date 
 limit 10
+
+-- Get the shortest 5 movies
+SELECT title FROM film
+ORDER BY length 
+limit 5
